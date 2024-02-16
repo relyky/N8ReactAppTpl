2e985d543303a9a7a4c53aaeec2350e1ca40c0e8
Rely Ky <relyky@hotmail.com>
2024-02-16 15:38:33 +0800

    add Banner

--- n8reactapptpl.client/src/MainLayout.tsx
+++ n8reactapptpl.client/src/MainLayout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Banner from "./Banner";
 
 export default function MainLayout() {
   //const navigation = useNavigation();
 
   return (
-    <main>
-      <h1>這是 MainLayout</h1>
-      <Outlet />
-    </main>
+    <>
+      <Banner />
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
