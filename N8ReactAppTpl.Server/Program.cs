df81c6adee3193acfcd641a64fc8a7d5fea46588
Rely Ky <relyky@hotmail.com>
2024-02-16 10:57:15 +0800

    setup Rin

--- N8ReactAppTpl.Server/Program.cs
+++ N8ReactAppTpl.Server/Program.cs
@@ -1,11 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.AddRinLogger(); // for Rin 監聽 HTTP 封包
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRin(); // for Rin 監聽 HTTP 封包
 
 var app = builder.Build();
 
@@ -15,8 +19,10 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+  app.UseRin(); // for Rin 監聽 HTTP 封包
   app.UseSwagger();
   app.UseSwaggerUI();
+  app.UseRinDiagnosticsHandler(); // for Rin 監聽 HTTP 封包
 }
 
 app.UseHttpsRedirection();
