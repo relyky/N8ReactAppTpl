f7481b550f20cb2677aee96af5fad6b08fb22bde
Rely Ky <relyky@hotmail.com>
2024-02-16 20:08:55 +0800

    custom theme

--- n8reactapptpl.client/src/App.tsx
+++ n8reactapptpl.client/src/App.tsx
@@ -1,11 +1,36 @@
+import { useReducer } from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { CssBaseline } from "@mui/material"
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material"
+import { zhTW } from '@mui/material/locale'
 import ErrorPage from "./error-page"
 import MainLayout from './MainLayout'
 import Login from './pages/Account/Login'
 import Counter from './pages/Counter/AppForm'
 import FetchData from './pages/FetchData/AppForm'
 import Home from './pages/Home/AppHome'
+
+//-----------------------------------------------------------------------------
+//## Resource
+
+const whiteTheme = createTheme(
+  {
+    palette: {
+      primary: { main: '#BF4690' },
+    },
+  },
+  zhTW, // Locale text:Use the theme to configure the locale text globally.
+);
+
+const darkTheme = createTheme(
+  {
+    palette: {
+      mode: 'dark',
+    },
+  },
+  zhTW, // Locale text:Use the theme to configure the locale text globally.
+);
+
+//-----------------------------------------------------------------------------
 
 const router = createBrowserRouter([
   {
@@ -25,10 +50,13 @@ const router = createBrowserRouter([
 ]);
 
 export default function App() {
+  //# 真正共享的實體在此建立
+  const [f_darkTheme, toggleTheme] = useReducer(mode => !mode, true)
+
   return (
-    <>
+    <ThemeProvider theme={f_darkTheme ? darkTheme : whiteTheme}>
       <CssBaseline />
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   )
 }
