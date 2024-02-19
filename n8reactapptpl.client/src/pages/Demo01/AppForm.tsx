8708f29e05544413a17e2b33aff22cefe4dd8489
Rely Ky <relyky@hotmail.com>
2024-02-19 11:09:37 +0800

    CSS isolation

--- n8reactapptpl.client/src/pages/Demo01/AppForm.tsx
+++ n8reactapptpl.client/src/pages/Demo01/AppForm.tsx
@@ -1,12 +1,17 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import Swal from "sweetalert2";
+import styles from './AppForm.module.css'
 
 export default function Demo01_AppForm() {
   return (
     <Container>
       <Typography variant='h3'>這是 Demo01</Typography>
-      <Box typography='body1'>測試 SweetAlert</Box>
+      <Box typography='body1'>測試 SweetAlert、CSS isolation.</Box>
       <Button variant='contained' onClick={handleSuccess}>success</Button>
+
+      <h3>這是 H3</h3>
+      <h3 className={styles.myh3}>這是 H3 with CSS isolation</h3>
+
     </Container>
   )
 
