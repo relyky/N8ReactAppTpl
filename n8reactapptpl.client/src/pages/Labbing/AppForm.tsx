e4d6b42953a92c634ffbccc84b926a8c2e8fc9a6
Rely Ky <relyky@hotmail.com>
2024-03-05 09:51:46 +0800

    Labbing01

--- n8reactapptpl.client/src/pages/Labbing/AppForm.tsx
+++ n8reactapptpl.client/src/pages/Labbing/AppForm.tsx
@@ -1,9 +1,13 @@
 import { Container, Typography } from "@mui/material";
+import Labbing01 from "./Labbing01";
 
 export default function Labbing_AppForm() {
   return (
     <Container>
       <Typography variant='h3'>Labbing 臨床實驗室</Typography>
+
+      <Labbing01 />
+
     </Container>
   )
 }
---
+++ n8reactapptpl.client/src/pages/Labbing/Labbing01.tsx
@@ -0,0 +1,49 @@
+import { Box } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  firstName: string
+  lastName: string
+};
+
+const initFormState: FormValues = {
+  firstName: '聰明',
+  lastName: '郝',
+}
+
+export default function Labbing01() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isValidating, isDirty },
+  } = useForm<FormValues>({ defaultValues: initFormState });
+
+  const onSubmit = handleSubmit((data) => alert(JSON.stringify(data)));
+
+  const handlePost = handleSubmit((data) => alert(JSON.stringify({ ...data, isPost: true })));
+
+  return (
+    <Box sx={{ border: 'red 2px solid' }}>
+      <h1>React Hook Form - useForm</h1>
+
+      <p>{`isValid:${isValid} | isValidating:${isValidating} | isDirty:${isDirty}`}</p>
+
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>First Name</label>
+          <input {...register("firstName", { required: "我是必填！" })} placeholder="Kotaro" />
+          {errors?.firstName && <p>{errors.firstName.message}</p>}
+        </div>
+
+        <div>
+          <label>Last Name</label>
+          <input {...register("lastName", { required: "我是必填2！" })} placeholder="Sugawara" />
+          {errors?.lastName && <p>{errors.lastName.message}</p>}
+        </div>
+
+        <input type="submit" />
+        <button onClick={handlePost} >送出</button>
+      </form>
+    </Box>
+  );
+}
