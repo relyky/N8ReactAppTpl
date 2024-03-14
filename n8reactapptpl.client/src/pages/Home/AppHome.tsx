b8e592e8d23068dd7a39f2b4d028c37396052b13
Rely Ky <relyky@hotmail.com>
2024-03-14 18:11:48 +0800

    Home

--- n8reactapptpl.client/src/pages/Home/AppHome.tsx
+++ n8reactapptpl.client/src/pages/Home/AppHome.tsx
@@ -1,10 +1,13 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 export default function Home_AppForm() {
   return (
     <Container>
       <Typography variant='h3'>這是首頁</Typography>
       <Box typography='body1' color='info.main'>首頁為特例不用登入</Box>
+
+      <Button href="login" size='large' sx={{ fontSize: '2rem' }}>{`前往登入》`}</Button>
+
     </Container>
   )
 }
