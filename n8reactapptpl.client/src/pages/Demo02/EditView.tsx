cf333906a59a4046f105dd00d1423b91da85b3f4
Rely Ky <relyky@hotmail.com>
2024-03-15 09:18:30 +0800

    FormStatePeeker

--- n8reactapptpl.client/src/pages/Demo02/EditView.tsx
+++ n8reactapptpl.client/src/pages/Demo02/EditView.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
-import { Button, Container, Toolbar, Typography } from "@mui/material";
+import { Button, Chip, Container, Paper, Stack, Toolbar, Typography } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { ATextField, FormContainer, FormRow, ReconfirmCommand, ResetCommand, SubmitCommand, ValidationSummary } from "../../highorder/formComponents/all";
+import { ATextField, FormContainer, FormRow, FormStatePeeker, ReconfirmCommand, ResetCommand, SubmitCommand, ValidationSummary } from "../../highorder/formComponents/all";
 import { IDemo02_FormData } from "../../DTO/Demo02/IDemo02_FormData";
 import { selectFormState, setEditMode } from "./useFormSlice";
 import useFormHand from "./useFormHand";
@@ -20,16 +20,34 @@ export default function EditView() {
       <Typography variant='h3' gutterBottom>CRUD-EditView</Typography>
 
       {Boolean(formData) && <FormContainer<IDemo02_FormData> values={formData}>
-        <FormRow>
-          <ATextField name='formNo' label='表單號碼' readOnly gridSize={[12, 3]} />
-          <ATextField name='formTitle' label='表單抬頭' required gridSize={[12, 9]}
-            minLength={[7, '表單抬頭 長度不可小於7。']} />
-          <ATextField name='expectDate' label='預計日期' placeholder='YYYY/MM/DD' />
-          <ATextField name='fieldA' label='欄位Ａ' />
-          <ATextField name='fieldB' label='欄位Ｂ' />
-          <ATextField name='fieldC' label='欄位Ｃ' />
-          <ATextField name='updDtm' label='更新時間' readOnly />
-        </FormRow>
+
+        <Paper sx={{ my: 2, p:2 }}>
+          <FormStatePeeker render={({
+            formState: { isDirty, isValidating, isValid, isSubmitting, isSubmitted, isSubmitSuccessful, submitCount }
+          }) => (
+            <Stack direction="row" spacing={1}>
+              <Chip label="isDirty" color="warning" disabled={!isDirty} />
+              <Chip label="isValidating" color="info" disabled={!isValidating} />
+              <Chip label="isValid" color="info" disabled={!isValid} />
+              <Chip label="isSubmitting" color="info" disabled={!isSubmitting} />
+              <Chip label={`isSubmitted (${submitCount})`} color="info" disabled={!isSubmitted} />
+              <Chip label="isSubmitSuccessful" color="success" disabled={!isSubmitSuccessful} />
+            </Stack>
+          )} />
+        </Paper>
+
+        <Paper sx={{ my: 2, p: 2 }}>
+          <FormRow>
+            <ATextField name='formNo' label='表單號碼' readOnly gridSize={[12, 3]} />
+            <ATextField name='formTitle' label='表單抬頭' required gridSize={[12, 9]}
+              minLength={[7, '表單抬頭 長度不可小於7。']} />
+            <ATextField name='expectDate' label='預計日期' placeholder='YYYY/MM/DD' />
+            <ATextField name='fieldA' label='欄位Ａ' />
+            <ATextField name='fieldB' label='欄位Ｂ' />
+            <ATextField name='fieldC' label='欄位Ｃ' />
+            <ATextField name='updDtm' label='更新時間' readOnly />
+          </FormRow>
+        </Paper>
 
         <Toolbar sx={{ gap: 2, justifyContent: 'center' }}>
           <SubmitCommand onSubmit={handler.updFormData} />
